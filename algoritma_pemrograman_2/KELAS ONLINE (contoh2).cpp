32841f06fd59394454843599d314202df9c40f53
cessyzulma <nshinhyezulma@gmail.com>
2020-04-14 15:25:07 +0700

    algoritma_pemrograman_2

---
+++ algoritma_pemrograman_2/KELAS ONLINE (contoh2).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main ()
+{
+	int a, b;
+
+    printf("Inputkan nilai a: ");
+    scanf("%i", &a);
+
+    printf("Inputkan nilai b: ");
+    scanf("%i", &b);
+
+    printf("Hasil a + b: %i\n", a + b);
+    printf("Hasil a - b: %i\n", a - b);
+    printf("Hasil a * b: %i\n", a * b);
+    printf("Hasil a / b: %i\n", a / b);
+    printf("Hasil a % b: %i\n", a % b);
+}
---
+++ algoritma_pemrograman_2/KELAS ONLINE (latihantugas).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main () 
+{
+	int x,y ;
+	x = 30;
+	y = 10;
+	cout<<"Hasil Pertambahan ="<<x+y<<endl;
+	cout<<"Hasil Pengurangan ="<<x-y<<endl;
+	cout<<"Hasil Perkalian ="<<x*y<<endl;
+	cout<<"Hasil Pembagian ="<<x/y<<endl;
+}
+	
